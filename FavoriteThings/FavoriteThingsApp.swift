5b04485e18fb0ced4e0164f3b2edd1a75de00472
Susie Wang <SWang@lcs.on.ca>
2024-12-11 12:31:59 -0500

    Initial Commit

---
+++ FavoriteThings/FavoriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingsApp.swift
+//  FavoriteThings
+//
+//  Created by Susie Wang on 2024-12-11.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ThingsListView()
+        }
+    }
+}
---
+++ FavoriteThings/Model/Views/ThingsView.swift
@@ -0,0 +1,26 @@
+//
+//  ThingsView.swift
+//  FavoriteThings
+//
+//  Created by Susie Wang on 2024-12-11.
+//
+
+import SwiftUI
+
+struct ThingsView: View {
+    
+    let providedThings: Things
+    
+    var body: some View{
+        VStack (alignment: .leading){
+            Text (providedThings.name)
+                .font(.largeTitle)
+            Text (providedThings.description)
+        }
+    }
+}
+
+#Preview {
+    ThingsView(providedThings: things )
+        .padding()
+}
